befeddd5823afdb60e48b8f976814e019d6a028e
Saad Zafar <saadzafar.code@gmail.com>
2024-11-29 07:39:52 +0100

    before react query

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
