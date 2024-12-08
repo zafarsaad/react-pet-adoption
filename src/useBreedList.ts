725e14d8d54c1f2045e5505ba2f90265d34fe990
Saad Zafar <saadzafar.code@gmail.com>
2024-12-08 03:03:22 +0100

    ending TypeScript Arc

---
+++ src/useBreedList.ts
@@ -0,0 +1,12 @@
+import { QueryStatus, useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
+import { Animal } from "./APIResponsesTypes";
+
+export default function useBreedList(animal: Animal) {
+  const results = useQuery(["breeds", animal], fetchBreedList);
+
+  return [results?.data?.breeds ?? [], results.status] as [
+    string[],
+    QueryStatus
+  ];
+}
